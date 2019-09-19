f90f5d66cf46a217e61c7c380b46e7fed62c1d84
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-19 10:26:18 -0500

    Incorporated error messages for failed input fields.

--- formik-practice/src/components/Form.js
+++ formik-practice/src/components/Form.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const LoginForm = ({ values, errors, touched }) => {
     return (
         <Form>
             <Field name="username" type="text" placeholder="Username" />
+            {errors.username && <p>{errors.username}</p>}
             <Field name="email" type="email" placeholder="E-mail" />
+            {errors.email && <p>{errors.email}</p>}
             <Field name="password" type="password" placeholder="Password" />
+            {errors.password && <p>{errors.password}</p>}
             <label>
                 <Field name="tos" type="checkbox" checked={values.tos} />
                 Accept Terms of Service
@@ -30,6 +34,18 @@ const FormikLoginForm = withFormik({
             tos: tos || false
         }
     },
+
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+            .min(6)
+            .required("A username is required"),
+        email: Yup.string()
+            .email()
+            .required("An E-mail is required"),
+        password: Yup.string()
+            .min(6)
+            .required("Invalid password")
+    }),
 
     handleSubmit(values) {
         console.log(values)
