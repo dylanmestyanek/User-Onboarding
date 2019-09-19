e5f4972d36d7209686c16b34b5a3963471e8247c
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-19 10:34:15 -0500

    Added touched response message to inputs from Yup. Console.log confirms axios.post request is going through successfully

--- formik-practice/src/components/Form.js
+++ formik-practice/src/components/Form.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
 const LoginForm = ({ values, errors, touched }) => {
     return (
         <Form>
             <Field name="username" type="text" placeholder="Username" />
-            {errors.username && <p>{errors.username}</p>}
+            {touched.username && errors.username && <p>{errors.username}</p>}
             <Field name="email" type="email" placeholder="E-mail" />
-            {errors.email && <p>{errors.email}</p>}
+            {touched.email && errors.email && <p>{errors.email}</p>}
             <Field name="password" type="password" placeholder="Password" />
-            {errors.password && <p>{errors.password}</p>}
+            {touched.password && errors.password && <p>{errors.password}</p>}
             <label>
                 <Field name="tos" type="checkbox" checked={values.tos} />
                 Accept Terms of Service
@@ -37,18 +38,22 @@ const FormikLoginForm = withFormik({
 
     validationSchema: Yup.object().shape({
         username: Yup.string()
-            .min(6)
+            .min(6, "Username must be longer than 6 characters")
             .required("A username is required"),
         email: Yup.string()
-            .email()
+            .email("E-mail is mandatory")
             .required("An E-mail is required"),
         password: Yup.string()
-            .min(6)
+            .min(6, "Password must be 6 characters or longer")
             .required("Invalid password")
     }),
 
-    handleSubmit(values) {
-        console.log(values)
+    handleSubmit() {
+        axios.post("https://reqres.in/api/users")
+            .then(respo => console.log(respo))
+            .catch(err => console.log("Whoopsies", err));
+
+        
     }
 })(LoginForm);
 
