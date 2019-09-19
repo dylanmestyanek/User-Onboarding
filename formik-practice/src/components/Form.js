2d7f70173e66847c56f9716aadb15f2fb2613fcf
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-09-19 14:24:20 -0500

    Styled basic layout of form, and submission button

--- formik-practice/src/components/Form.js
+++ formik-practice/src/components/Form.js
@@ -17,25 +17,34 @@ const LoginForm = ({
     }, [status])
 
     return (
-        <Form>
-            <Field name="username" type="text" placeholder="Username" />
-            {touched.username && errors.username && <p>{errors.username}</p>}
-            <Field name="email" type="email" placeholder="E-mail" />
-            {touched.email && errors.email && <p>{errors.email}</p>}
-            <Field name="password" type="password" placeholder="Password" />
-            {touched.password && errors.password && <p>{errors.password}</p>}
-            <Field name="role" component="select">
-                <option>Please select a role</option>
-                <option name="member">Team Member</option>
-                <option name="lead">Team Lead</option>
-                <option name="admin">Admin</option>
-            </Field>
+        <Form className="user-form">
+            <div className="input-container">
+                <div>
+                    <Field name="username" type="text" placeholder="Username" />
+                    {touched.username && errors.username && <p className="error-message">{errors.username}</p>}
+                </div>
+                <div>
+                    <Field name="email" type="email" placeholder="E-mail" />
+                    {touched.email && errors.email && <p className="error-message">{errors.email}</p>}
+                </div>
+                <div>
+                    <Field name="password" type="password" placeholder="Password" />
+                    {touched.password && errors.password && <p className="error-message">{errors.password}</p>}
+                </div>
+                <Field name="role" component="select">
+                    <option>Please select a role</option>
+                    <option name="member">Team Member</option>
+                    <option name="lead">Team Lead</option>
+                    <option name="admin">Admin</option>
+                </Field>
+            </div>
+            <br/>
             <label>
                 <Field name="tos" type="checkbox" checked={values.tos} />
                 Accept Terms of Service
             </label>
-            {errors.tos && <p>{errors.tos}</p>}
-            <button type="submit">Submit</button>
+            {errors.tos && <p className="error-message">{errors.tos}</p>}
+            <button type="submit" className="submit-button">Submit</button>
         </Form>
     );
 };
@@ -62,15 +71,14 @@ const FormikLoginForm = withFormik({
             .min(6, "Username must be longer than 6 characters")
             .required("A username is required"),
         email: Yup.string()
-            .email("E-mail is mandatory")
-            .required("An E-mail is required"),
+            .email("Invalid E-mail address")
+            .required("E-mail is required"),
         password: Yup.string()
             .min(6, "Password must be 6 characters or longer")
             .required("Invalid password"),
         tos: Yup
             .boolean()
             .required("Must Accept Terms of Service")
-            // .oneOf([true], 'Must Accept Terms of Service')
     }),
 
     handleSubmit(values, { setStatus, setErrors }) {
